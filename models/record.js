19ac3944767861ae924d1175eecdaf95e427969e
Diana Blanco <dbp@Dianas-MacBook-Pro.local>
2018-09-20 20:07:28 +0200

    add the record Schema

---
+++ models/record.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const recordSchema = new Schema({
+  owner: {
+    type: String,
+    required: true
+  },
+  recordName: {
+    type: String,
+    required: true
+  },
+  isActive: {
+    type: Boolean,
+    required: true
+  },
+  artist: {
+    type: String,
+    required: true
+  },
+  coverImageURL: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: String,
+    enum: ['country', 'pop', 'rock', 'metal', 'hip-hop', 'punk', 'alternative'],
+    required: true
+  },
+  releaseYear: {
+    type: Number,
+    required: true
+  },
+  condition: {
+    type: String,
+    enum: ['great', 'as new', 'good', 'used', 'scratched'],
+    required: true
+  }
+})
+
+const Record = mongoose.model('Record', recordSchema)
+
+module.exports = Record
