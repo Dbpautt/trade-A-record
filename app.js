97f36f52f761aca49e2ae60fcbcc713021a3f1a9
Diana Blanco <dbp@Dianas-MacBook-Pro.local>
2018-09-20 19:17:48 +0200

    add authentication log in and signup and logout

--- app.js
+++ app.js
@@ -4,24 +4,49 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const mongoose = require('mongoose')
+const flash = require('connect-flash')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
+const authRouter = require('./routes/auth')
 
 const app = express()
+
+// -- connect to mongoose
+mongoose.Promise = Promise
+mongoose.connect('mongodb://localhost/trade-a-record', {
+  keepAlive: true,
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  }),
+  secret: 'some-string',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000
+  }
+}))
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(flash())
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/auth', authRouter)
 
 app.use((req, res, next) => {
   res.status(404)
