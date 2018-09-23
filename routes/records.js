449c026004cecc2063d62a8315b56fd89b1f528a
jsplmns <jonathan@spielmannsbilder.de>
2018-09-23 14:14:08 +0200

    trade page initialized

--- routes/records.js
+++ routes/records.js
@@ -6,6 +6,7 @@ const router = express.Router()
 // const ObjectId = require('mongoose').Types.ObjectId;
 
 const Records = require('../models/record')
+const Trade = require('../models/trade')
 // const User = require('../models/user')
 
 /* GET records page. */
@@ -15,12 +16,11 @@ router.get('/', (req, res, next) => {
       const data = {
         records: results
       }
+      res.render('records', data)
     })
     .catch((error) => {
       console.log('there has been an error', error)
     })
-
-  res.render('records', data)
 })
 
 /* GET records detail page. */
@@ -42,6 +42,27 @@ router.get('/:recordId', (req, res, next) => {
       return res.redirect('/records')
     })
 })
+
+router.post('/:recordId/:ownerId/trade', (req, res, next) => {
+  const id = req.params.recordId
+  const owner = req.params.ownerId
+  // const { status, recordRequested, recordOffered, requestMaker, requestApprover } = req.body;
+
+  // if (!name || !studentName || !presentationURL || !projectURL || !imageURL) {
+  //   req.flash('project-form-error', 'all fields are mandatory');
+  //   req.flash('project-form-data', { name, studentName, presentationURL, projectURL, imageURL });
+  //   return res.redirect('/projects/create');
+  // }
+
+  const trade = new Trade({ status: 'pending', recordRequested: id, recordOffered: req.session.currentUser._id, requestMaker: req.session.currentUser._id, requestApprover: owner })
+  trade.save()
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch(next)
+})
+
+// _______________________________________________________
 
 router.get('/:recordId/trade', (req, res, next) => {
   const id = req.params.recordId
