dac1c05002b078d7c2b4fe216d6856d6eaa55535
Barbara Koehler <barkoehler@gmail.com>
2018-09-26 14:53:08 -0400

    changed data in seed files for presentation

--- data/records.js
+++ data/records.js
@@ -1,114 +1,114 @@
 'use strict';
 
 const records = [{
-  owner: ['user1'],
+  owner: ['Diana'],
   name: 'Legend',
   isActive: true,
   artist: 'Bob Marley',
   coverImageURL: 'https://www.elclubdelrockandroll.com/uploads/7/6/7/9/76793449/s132834213532544176_p1823_i1_w440.jpeg',
-  description: 'this is the description',
-  genre: 'alternative',
-  releaseYear: 1992,
-  condition: 'as new'
+  description: 'A greatest hits collection of singles in its original vinyl format and is the best-selling reggae album of all-time',
+  genre: 'Alternative',
+  releaseYear: 1984,
+  condition: 'Scratched'
 },
 {
-  owner: ['user1'],
+  owner: ['Jonathan'],
   name: 'The times they are a-changin’',
   isActive: true,
   artist: 'Bob Dylan',
   coverImageURL: 'https://i.pinimg.com/originals/75/74/55/757455cfd4c0e9b6ff54e99c7ce582b4.jpg',
-  description: 'this is the description',
-  genre: 'alternative',
-  releaseYear: 1992,
-  condition: 'as new'
+  description: 'Come gather around people Wherever you roam and admit that the waters around you have grown',
+  genre: 'Rock',
+  releaseYear: 1964,
+  condition: 'Good'
 },
 {
-  owner: ['user1'],
+  owner: ['Jonathan'],
   name: 'Help!',
   isActive: true,
   artist: 'The Beatles',
   coverImageURL: 'http://1.bp.blogspot.com/-l6t_-SYA8k8/VJVxtyxIEuI/AAAAAAAAPFU/KnR4WhjUns4/s1600/help_LP_cover.jpg',
-  description: 'this is the description',
-  genre: 'alternative',
-  releaseYear: 1992,
-  condition: 'as new'
+  description: 'Produced by George Martin, this was the fifth UK album release by the band, and contains fourteen songs in its original British form',
+  genre: 'Pop',
+  releaseYear: 1965,
+  condition: 'As new'
 },
 {
-  owner: ['user2'],
+  owner: ['Yenderly'],
   name: 'G.I. Blues',
   isActive: true,
   artist: 'Elvis',
   coverImageURL: 'http://static.flickr.com/31/49567889_48b7a4ba44.jpg',
-  description: 'this is the description',
+  description: 'G.I. Blues is a 1960 American musical comedy film directed by Norman Taurog and starring Elvis Presley, Juliet Prowse, and Robert Ivers',
   genre: 'alternative',
-  releaseYear: 1992,
-  condition: 'as new'
+  releaseYear: 1960,
+  condition: 'Used'
 },
 {
-  owner: ['user1'],
+  owner: ['Barbara'],
   name: 'Justice',
   isActive: true,
   artist: 'Justice',
   coverImageURL: 'https://i.pinimg.com/originals/8a/4a/43/8a4a43ec0a71c1125ee4a46d67bc32c1.jpg',
-  description: 'this is the description',
-  genre: 'alternative',
-  releaseYear: 1992,
-  condition: 'as new'
+  description: 'These guys are just so cool',
+  genre: 'Alternative',
+  releaseYear: '2007',
+  condition: 'As new'
 },
 {
-  owner: ['user2'],
+  owner: ['Axel'],
   name: 'Nevermind',
   isActive: true,
   artist: 'Nirvana',
   coverImageURL: 'https://www.fiftiesstore.com/media/catalog/product/cache/c687aa7517cf01e65c009f6943c2b1e9/b/e/bert_e71073_xxl.jpg',
-  description: 'this is the description',
-  genre: 'alternative',
-  releaseYear: 1992,
-  condition: 'as new'
+  description: 'Twenty-seven years after its release, Nirvana\'s Nevermind remains one of the most exhilarating and pivotal albums of all time.',
+  genre: 'Alternative',
+  releaseYear: 1991,
+  condition: 'Good'
 },
 {
-  owner: ['user2'],
+  owner: ['Franci'],
   name: 'Free the Universe',
   isActive: true,
   artist: 'Major Lazer',
   coverImageURL: 'https://is5-ssl.mzstatic.com/image/thumb/Music/v4/2a/23/00/2a230004-37f2-4f96-7e3a-33078afc7541/656605029276.jpg/1200x630bb.jpg',
-  description: 'this is the description',
-  genre: 'alternative',
-  releaseYear: 1992,
-  condition: 'as new'
+  description: 'Diplo has risen through the ranks and asserted himself as one of the most pioneering producers of the time.',
+  genre: 'Alternative',
+  releaseYear: 2012,
+  condition: 'As new'
 },
 {
-  owner: ['user1'],
+  owner: ['Diana'],
   name: 'Dr Doom',
   isActive: true,
   artist: 'Kool Keith',
   coverImageURL: 'https://img.discogs.com/HjHrXZejK8rDj2hV5n3PErreZVs=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-140518-1449607945-2777.jpeg.jpg',
-  description: 'this is the description',
-  genre: 'alternative',
-  releaseYear: 1992,
-  condition: 'as new'
+  description: 'Dr Octagon!',
+  genre: 'Hip-hop',
+  releaseYear: 1999,
+  condition: 'Good'
 },
 {
-  owner: ['user1'],
+  owner: ['Barbara'],
   name: 'Sex Style',
   isActive: true,
   artist: 'Kool Keith',
   coverImageURL: 'http://cdn.shopify.com/s/files/1/0993/9646/products/KTR006CD.jpeg?v=1466276875',
-  description: 'this is the description',
-  genre: 'alternative',
-  releaseYear: 1992,
-  condition: 'as new'
+  description: 'More of the good stuff from Dr Octagon',
+  genre: 'Hip-hop',
+  releaseYear: 1999,
+  condition: 'Scratched'
 },
 {
-  owner: ['user1'],
+  owner: ['Jonathan'],
   name: 'Reign In Blood',
   isActive: true,
   artist: 'Slayer',
   coverImageURL: 'https://najlepszamuzyka.pl/17483-large_default/slayer-reign-in-blood-1-lp-180-gram-pressing-wydanie-amerykanskie.jpg',
-  description: 'this is the description',
-  genre: 'alternative',
-  releaseYear: 1992,
-  condition: 'as new'
+  description: 'The 1986 headbanger released on Def Jam. reign in Blood.',
+  genre: 'Alternative',
+  releaseYear: 1986,
+  condition: 'As new'
 }];
 
 module.exports = records;
